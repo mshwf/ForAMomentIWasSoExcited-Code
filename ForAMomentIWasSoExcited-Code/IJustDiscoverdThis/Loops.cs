e0b1f82ea28661aefb884314895b6615a0cda471
Mohamed Elshawaf <m_shawaf@outlook.com>
2019-09-27 13:31:24 +0200

    initial commit

--- ForAMomentIWasSoExcited-Code/IJustDiscoverdThis/Loops.cs
+++ ForAMomentIWasSoExcited-Code/IJustDiscoverdThis/Loops.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace ForAMomentIWasSoExcited_Code.IJustDiscoverdThis
 {
     class Loops
     {
+        //loop through 2D array, row by row
         public static void LoopThrough2DArray(char[,] locs)
         {
-            var rowMax = locs.GetLength(0);
-            var colMax = locs.GetLength(1);
-            for (int row = 0, col = 0; row < rowMax && col < colMax; Iterator(ref row, rowMax, ref col, colMax))
+            var rowsLength = locs.GetLength(0);
+            var colsLength = locs.GetLength(1);
+            for (int row = 0, col = 0; row < rowsLength && col < colsLength; Iterator(ref row, rowsLength, ref col, colsLength))
             {
                 Debug.WriteLine($"[{row}, {col}]");
                 Debug.WriteLine("------");
             }
         }
 
-        private static void Iterator(ref int row, int rowMax, ref int col, int colMax)
+        private static void Iterator(ref int row, int rowsLength, ref int col, int colsLength)
         {
-            if (row < rowMax) col++;
-            if (col == colMax)
+            if (row < rowsLength) col++;
+            if (col == colsLength)
             {
                 col = 0;
                 row++;
